cfd5f737234619c424a29b9c204e228c7723e0e7
JacobDev1 <contact@codepoems.eu>
2023-09-02 08:17:41 +0200

    Replace format-specific naming

--- OutputTab.py
+++ OutputTab.py
@@ -126,31 +126,31 @@ class OutputTab(QWidget):
         self.format_cmb.addItems(("JPEG XL","AVIF", "WEBP", "JPG", "PNG", "Smallest Lossless"))
         self.format_cmb.currentIndexChanged.connect(self.onFormatChange)
 
-        self.format_jxl_e_sb = QSpinBox()
-        self.format_jxl_e_sb.setRange(1,9)
+        self.format_e_sb = QSpinBox()
+        self.format_e_sb.setRange(1,9)
         self.format_e_l = QLabel("Effort")
         self.format_jxl_e_int_cb = QCheckBox("Intelligent")
         self.format_jxl_e_int_cb.toggled.connect(self.toggleEffort)
 
-        self.format_jxl_q_sb = QSpinBox()
-        self.format_jxl_q_sb.setRange(1,100)
+        self.format_q_sb = QSpinBox()
+        self.format_q_sb.setRange(1,100)
         
-        self.format_jxl_q_sl = QSlider(Qt.Horizontal)
-        self.format_jxl_q_sl.setRange(1, 100)
-        self.format_jxl_q_sl.setTickPosition(QSlider.TicksBelow)
-        self.format_jxl_q_sl.setTickInterval(5)
-        self.format_jxl_q_sl.valueChanged.connect(self.onQualitySlChange)
-        self.format_jxl_q_sb.valueChanged.connect(self.onQualitySbChange)
+        self.format_q_sl = QSlider(Qt.Horizontal)
+        self.format_q_sl.setRange(1, 100)
+        self.format_q_sl.setTickPosition(QSlider.TicksBelow)
+        self.format_q_sl.setTickInterval(5)
+        self.format_q_sl.valueChanged.connect(self.onQualitySlChange)
+        self.format_q_sb.valueChanged.connect(self.onQualitySbChange)
 
         # Lossless
         self.format_lossless_if_cb = QCheckBox("Lossless (only if smaller)")
         self.format_lossless_if_cb.toggled.connect(self.toggleLossless)
-        self.format_jxl_q_lossless_cb = QCheckBox("Lossless")
-        self.format_jxl_q_lossless_cb.toggled.connect(self.toggleLossless)
+        self.format_lossless_cb = QCheckBox("Lossless")
+        self.format_lossless_cb.toggled.connect(self.toggleLossless)
         self.format_max_efc = QCheckBox("Max Efficiency")
         self.format_max_efc.setVisible(False)
         format_lossless_hbox = QHBoxLayout()
-        format_lossless_hbox.addWidget(self.format_jxl_q_lossless_cb)
+        format_lossless_hbox.addWidget(self.format_lossless_cb)
         format_lossless_hbox.addWidget(self.format_lossless_if_cb)
         format_lossless_hbox.addWidget(self.format_max_efc)
 
@@ -162,13 +162,13 @@ class OutputTab(QWidget):
         format_jxl_e_hbox = QHBoxLayout()
         format_jxl_e_hbox.addWidget(self.format_e_l)
         format_jxl_e_hbox.addWidget(self.format_jxl_e_int_cb)
-        format_jxl_e_hbox.addWidget(self.format_jxl_e_sb)
+        format_jxl_e_hbox.addWidget(self.format_e_sb)
         format_grp_layout.addLayout(format_jxl_e_hbox)
 
         format_jxl_q_hbox = QHBoxLayout()
         format_jxl_q_hbox.addWidget(self.format_q_l)
-        format_jxl_q_hbox.addWidget(self.format_jxl_q_sl)
-        format_jxl_q_hbox.addWidget(self.format_jxl_q_sb)
+        format_jxl_q_hbox.addWidget(self.format_q_sl)
+        format_jxl_q_hbox.addWidget(self.format_q_sb)
         format_grp_layout.addLayout(format_jxl_q_hbox)
         
         output_page_lt.addWidget(format_grp,0,1)
@@ -207,11 +207,11 @@ class OutputTab(QWidget):
     def getSettings(self):
         parameters = {
             "format": self.format_cmb.currentText(),
-            "quality": self.format_jxl_q_sb.value(),
-            "lossless": self.format_jxl_q_lossless_cb.isChecked(),
+            "quality": self.format_q_sb.value(),
+            "lossless": self.format_lossless_cb.isChecked(),
             "lossless_if_smaller": self.format_lossless_if_cb.isChecked(),
             "max_efficiency": self.format_max_efc.isChecked(),
-            "effort": self.format_jxl_e_sb.value(),
+            "effort": self.format_e_sb.value(),
             "intelligent_effort": self.format_jxl_e_int_cb.isChecked(),
             "if_file_exists": self.if_file_exists_cmb.currentText(),
             "custom_output_dir": self.choose_output_ct_rb.isChecked(),
@@ -251,15 +251,15 @@ class OutputTab(QWidget):
     def onFormatChange(self):
         cur_format = self.format_cmb.currentText()
         
-        self.format_jxl_q_lossless_cb.setChecked(False)     # It needs to in the beginning
+        self.format_lossless_cb.setChecked(False)     # It needs to in the beginning
         self.format_lossless_if_cb.setChecked(False)        # Widgets reenable themselves when you use setChecked() on a disabled widget
 
         # Enable Lossless Mode
         if cur_format in ("JPEG XL", "WEBP"):
-            self.format_jxl_q_lossless_cb.setEnabled(True)
+            self.format_lossless_cb.setEnabled(True)
             self.format_lossless_if_cb.setEnabled(True)
         else:
-            self.format_jxl_q_lossless_cb.setEnabled(False)
+            self.format_lossless_cb.setEnabled(False)
             self.format_lossless_if_cb.setEnabled(False)
 
         # Enable Intelligent Effort Settings
@@ -271,34 +271,34 @@ class OutputTab(QWidget):
         
         # Effort / Speed
         if cur_format in ("JPEG XL", "AVIF"):
-            self.format_jxl_e_sb.setEnabled(True)
+            self.format_e_sb.setEnabled(True)
         else:
-            self.format_jxl_e_sb.setEnabled(False)
+            self.format_e_sb.setEnabled(False)
 
         # Disable Quality Slider
         if cur_format in ("PNG", "Smallest Lossless"):
-            self.format_jxl_q_sb.setEnabled(False)
-            self.format_jxl_q_sl.setEnabled(False)
+            self.format_q_sb.setEnabled(False)
+            self.format_q_sl.setEnabled(False)
         else:
-            self.format_jxl_q_sb.setEnabled(True)
-            self.format_jxl_q_sl.setEnabled(True)
+            self.format_q_sb.setEnabled(True)
+            self.format_q_sl.setEnabled(True)
         
         # Quality Slider Range and label
         if cur_format == "AVIF":
-            self.format_jxl_q_sl.setRange(-63, 0)
-            self.format_jxl_q_sb.setRange(0, 63)
-            self.format_jxl_e_sb.setRange(0, 10)
-            self.format_jxl_e_sb.setValue(6)
-            self.format_jxl_q_sl.setValue(-20)
+            self.format_q_sl.setRange(-63, 0)
+            self.format_q_sb.setRange(0, 63)
+            self.format_e_sb.setRange(0, 10)
+            self.format_e_sb.setValue(6)
+            self.format_q_sl.setValue(-20)
             self.format_q_l.setText("Constant Quality")
             self.format_e_l.setText("Speed")
             self.format_jxl_e_int_cb.setText("Best Quality")
         else:
-            self.format_jxl_q_sl.setRange(1, 100)
-            self.format_jxl_q_sb.setRange(1, 100)
-            self.format_jxl_e_sb.setRange(1, 9)
-            self.format_jxl_e_sb.setValue(7)
-            self.format_jxl_q_sl.setValue(80)
+            self.format_q_sl.setRange(1, 100)
+            self.format_q_sb.setRange(1, 100)
+            self.format_e_sb.setRange(1, 9)
+            self.format_e_sb.setValue(7)
+            self.format_q_sl.setValue(80)
             self.format_q_l.setText("Quality")
             self.format_e_l.setText("Effort")
             self.format_jxl_e_int_cb.setText("Intelligent")
@@ -316,43 +316,43 @@ class OutputTab(QWidget):
             self.setEffortVisible(True)
 
     def onQualitySlChange(self):
-        self.format_jxl_q_sb.setValue(abs(self.format_jxl_q_sl.value()))
+        self.format_q_sb.setValue(abs(self.format_q_sl.value()))
 
     def onQualitySbChange(self):
         if self.format_cmb.currentText() == "AVIF":
-            self.format_jxl_q_sl.setValue(-self.format_jxl_q_sb.value())
+            self.format_q_sl.setValue(-self.format_q_sb.value())
         else:
-            self.format_jxl_q_sl.setValue(self.format_jxl_q_sb.value())
+            self.format_q_sl.setValue(self.format_q_sb.value())
 
     def toggleEffort(self):
-        if self.format_jxl_e_sb.isEnabled():
-            self.format_jxl_e_sb.setEnabled(False)
+        if self.format_e_sb.isEnabled():
+            self.format_e_sb.setEnabled(False)
         else:
-            self.format_jxl_e_sb.setEnabled(True)
+            self.format_e_sb.setEnabled(True)
 
     def toggleLossless(self):
-        lossless = self.format_jxl_q_lossless_cb.isChecked()
+        lossless = self.format_lossless_cb.isChecked()
         lossless_if = self.format_lossless_if_cb.isChecked()
 
         if lossless:
-            self.format_jxl_q_sl.setEnabled(False)
-            self.format_jxl_q_sb.setEnabled(False)
+            self.format_q_sl.setEnabled(False)
+            self.format_q_sb.setEnabled(False)
             self.format_lossless_if_cb.setEnabled(False)
         elif lossless_if:
-            self.format_jxl_q_lossless_cb.setEnabled(False)
+            self.format_lossless_cb.setEnabled(False)
         else:
-            self.format_jxl_q_sl.setEnabled(True)
-            self.format_jxl_q_sb.setEnabled(True)
+            self.format_q_sl.setEnabled(True)
+            self.format_q_sb.setEnabled(True)
             self.format_lossless_if_cb.setEnabled(True)
-            self.format_jxl_q_lossless_cb.setEnabled(True)
+            self.format_lossless_cb.setEnabled(True)
 
     def resetToDefault(self):
         if self.format_cmb.currentText() == "AVIF":
-            self.format_jxl_e_sb.setValue(6)
-            self.format_jxl_q_sl.setValue(-20)
+            self.format_e_sb.setValue(6)
+            self.format_q_sl.setValue(-20)
         else:
-            self.format_jxl_e_sb.setValue(7)
-            self.format_jxl_q_sl.setValue(80)
+            self.format_e_sb.setValue(7)
+            self.format_q_sl.setValue(80)
 
         self.choose_output_src.setChecked(True)
         
@@ -364,7 +364,7 @@ class OutputTab(QWidget):
         self.if_file_exists_cmb.setCurrentIndex(1)
 
         # Lossless
-        self.format_jxl_q_lossless_cb.setChecked(False)
+        self.format_lossless_cb.setChecked(False)
         self.format_lossless_if_cb.setChecked(False)
         self.format_max_efc.setChecked(False)
 
@@ -380,11 +380,11 @@ class OutputTab(QWidget):
     
     def setEffortVisible(self, visible):
         self.format_e_l.setVisible(visible)
-        self.format_jxl_e_sb.setVisible(visible)
+        self.format_e_sb.setVisible(visible)
         self.format_e_l.setVisible(visible)
         self.format_jxl_e_int_cb.setVisible(visible)
     
     def setLosslessVisible(self, visible):
         self.format_lossless_if_cb.setVisible(visible)
-        self.format_jxl_q_lossless_cb.setVisible(visible)
+        self.format_lossless_cb.setVisible(visible)
         self.format_lossless_if_cb.setVisible(visible)
