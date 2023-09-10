ddf9ec6ab41967464e18f220804c086e110bb9ce
JacobDev1 <contact@codepoems.eu>
2023-09-10 09:56:00 +0200

    Implement downscaling

--- Convert.py
+++ Convert.py
@@ -1,4 +1,4 @@
-import os, random, subprocess
+import os, random, subprocess, shutil
 from send2trash import send2trash
 from VARIABLES import DEBUG, IMAGE_MAGICK_PATH
 
@@ -90,40 +90,97 @@ class Convert():
                 if paths[i] != new_path:
                     os.rename(paths[i], new_path)
     
-    # Prototype functions for resizing
-    # def downscaleByPercent(self, src, dst, amount=10, n=None):
-    #     """Resize the image by percentage. Keeps the same aspect ratio."""
-    #     args = [
-    #         f"-resize {100 - amount}%"
-    #     ]
+    def downscaleByPercent(self, src, dst, amount=10, n=None):
+        """Resize the image by percentage. Keeps the same aspect ratio."""
+        args = [
+            f"-resize {100 - amount}%"
+        ]
 
-    #     self.convert(IMAGE_MAGICK_PATH, src, dst, args, n)
+        self.convert(IMAGE_MAGICK_PATH, src, dst, args, n)
 
-    # def downscaleToMaxPixelSize(self, src, dst, max_w, max_h, n=None):
-    #     args = [
-    #         f"-resize {max_w}x{max_h}"
-    #     ]
+    def downscaleToMaxRes(self, src, dst, max_w, max_h, n=None):
+        args = [
+            f"-resize {max_w}x{max_h}"
+        ]
 
-    #     self.convert(IMAGE_MAGICK_PATH, src, dst, args, n)
+        self.convert(IMAGE_MAGICK_PATH, src, dst, args, n)
+            
+    def downscaleToMaxFileSize(self, params):
+        """Downscale image to fit under a certain file size."""
+
+        # Prepare data
+        amount = params["step"]
+        proxy_src = self.getUniqueFilePath(params["dst_dir"], params["name"], "png", True)
+        shutil.copy(params["src"], proxy_src)
+
+        # Downscale until it's small enough
+        while True:
+            # Normal conversion
+            self.convert(params["enc"], proxy_src, params["dst"], params["args"], params["n"])
+
+            # Cap amount
+            if amount == 99:
+                self.delete(proxy_src)
+                return False
+
+            # If bigger - resize
+            if (os.path.getsize(params["dst"]) / 1024) > params["max_size"]:
+                amount += params["step"]
+                if amount > 100:
+                    amount = 99 # Cap amount
+                    self.log("[Error] Cannot downscale to less than 1%", params["n"])
+                    # return False
+                
+                self.downscaleByPercent(params["src"], proxy_src, amount, params["n"])
+            else:
+                # JPEG XL - intelligent effort
+                if params["jxl_int_e"]: 
+                    params["args"][1] = "-e 9"
+                    e9_tmp = self.getUniqueFilePath(params["dst_dir"], params["name"], "jxl", True)
+                    self.convert(params["enc"], proxy_src, e9_tmp, params["args"], params["n"])
+                    if os.path.getsize(e9_tmp) < os.path.getsize(params["dst"]):
+                        self.delete(params["dst"])
+                        os.rename(e9_tmp, params["dst"])
+                    else:
+                        self.delete(e9_tmp)
+
+                # Clean-up
+                self.delete(proxy_src)
+                return True
     
-    # Sample implementation. To be moved into Worker.py and made to calc size based on desired output format (e.g. AVIF)
-    # def downscaleToMaxFileSize(self, src, dst, max_size, step=10, n=None):
-    #     """Downscale image to fit under a certain file size.
+    def downscale(self, params):
+        """A wrapper for all downscaling methods.
+        
+            "mode" - downscaling mode
+            "enc" - encoder path
+            "jxl_int_e" - An exception to handle intelligent effort
+            "src" - source PNG absolute path
+            "dst" - destination absolute path
+            "dst_dir": - destination directory
+            "name" - item name
+            "args" - encoder arguments
 
-    #         max_size - takes KiB (e.g. 500 KiB)
-    #         step - takes % (e.g. 10%). Keep between 5% - 10%
-    #     """
-    #     amount = step
-    #     while True:
-    #         self.downscaleByPercent(src, dst, amount, n)
+            Max File Size
+            "step" - takes % (e.g. 10%). Keep between 5% - 20%
+            "max_size" - desired size - takes KiB (e.g. 500 KiB)
 
-    #         print(f"[DEBUG] {amount}; {os.path.getsize(dst) / 1024}")
-    #         if (os.path.getsize(dst) / 1024) > max_size:
-    #             amount += step
-    #             if amount < 1:
-    #                 self.log("[Error] Cannot downscale to less than 1%", n)
-    #                 return False
-    #             self.delete(dst)
-    #             continue
-    #         else:
-    #             return True
+            Percent
+            "percent" - downscale by that amount
+
+            Max Size
+            "width" - max width in px
+            "height" - max height in px
+            
+            Misc
+            "resample": - resampling method
+            "n" - worker number
+        """
+        match params["mode"]:
+            case "Max File Size":
+                self.downscaleToMaxFileSize(params)
+            case "Percent":
+                self.downscaleByPercent(params["src"], params["dst"], 100 - params["percent"], params["n"])
+            case "Max Resolution":
+                self.downscaleToMaxRes(params["src"], params["dst"], params["width"], params["height"], params["n"])
+            case _:
+                self.log(f"[Error] Downscaling mode not recognized ({params['mode']})")
