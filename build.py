5c6abfc779f5e975f7f1f90d0d4142492a510184
JacobDev1 <contact@codepoems.eu>
2023-09-17 17:51:45 +0200

    Improve pathing

--- build.py
+++ build.py
@@ -9,6 +9,7 @@ PROGRAM_FOLDER = os.path.dirname(os.path.realpath(__file__))
 
 def replaceLine(path, pattern, new_line):
     """Replace the first line containing a pattern."""
+    path = os.path.normpath(path)
     if not os.path.isfile(path):
         return
 
@@ -26,6 +27,23 @@ def replaceLine(path, pattern, new_line):
     
     with open(path, "w") as file:
         file.writelines(content)
+
+def copy(src, dst):
+    src = os.path.normpath(src)
+    dst = os.path.normpath(dst)
+
+    try:
+        shutil.copy(src, dst)
+    except OSError:
+        print(f"[Error] Copying failed ({src} -> {dst})")
+
+def makedirs(path):
+    path = os.path.normpath(path)
+
+    try:
+        os.makedirs(path)
+    except OSError:
+        print(f"[Error] Makedirs failed ({path})")
 
 if __name__ == '__main__':
     # Clean
@@ -53,7 +71,7 @@ if __name__ == '__main__':
 
     # Preapre Directory and Build Binaries
     print("[Building] Generating binaries")
-    os.makedirs("dist")
+    makedirs("dist")
     PyInstaller.__main__.run([
         'main.spec'
     ])
@@ -61,31 +79,31 @@ if __name__ == '__main__':
     # Copy Dependencies
     print("[Building] Copying dependencies")
     if platform.system() == "Windows":
-        os.makedirs(f"dist/{PROGRAM_NAME}/bin/win/")
+        makedirs(f"dist/{PROGRAM_NAME}/bin/win/")
         for i in glob.glob("bin/win/*"):
-            shutil.copy(i, f"dist/{PROGRAM_NAME}/bin/win/")
+            copy(i, f"dist/{PROGRAM_NAME}/bin/win/")
     elif platform.system() == "Linux":
-        os.makedirs(f"dist/{PROGRAM_NAME}/bin/linux/")
+        makedirs(f"dist/{PROGRAM_NAME}/bin/linux/")
         for i in glob.glob("bin/linux/*"):
-            shutil.copy(i, f"dist/{PROGRAM_NAME}/bin/linux/")
+            copy(i, f"dist/{PROGRAM_NAME}/bin/linux/")
 
     # Append an Installer
     print("[Building] Appending an installer")
     if platform.system() == "Linux":
-        shutil.copy("misc/install.sh","dist")
-        shutil.copy("misc/xl-converter.desktop","dist")
+        copy("misc/install.sh","dist")
+        copy("misc/xl-converter.desktop","dist")
     elif platform.system() == "Windows":
-        shutil.copy("misc/install.iss","dist")
+        copy("misc/install.iss","dist")
 
     # Embed the Version Number
     print("[Building] Embedding the version number")
     if platform.system() == "Linux":
-        replaceLine(os.path.join(PROGRAM_FOLDER, "dist/install.sh"), "VERSION=", f"VERSION=\"{VERSION}\"\n")
+        replaceLine(f"{PROGRAM_FOLDER}/dist/install.sh", "VERSION=", f"VERSION=\"{VERSION}\"\n")
     elif platform.system() == "Windows":
-        replaceLine(os.path.join(PROGRAM_FOLDER, "dist/install.iss"), "#define MyAppVersion", f"#define MyAppVersion \"{VERSION}\"\n")
+        replaceLine(f"{PROGRAM_FOLDER}/dist/install.iss", "#define MyAppVersion", f"#define MyAppVersion \"{VERSION}\"\n")
 
     # Log Last Build Platform
     with open("build/last_built_on","w") as last_built_on:
         last_built_on.write(f"{platform.system()}_{platform.architecture()}")
 
-    print(f"[Building] Finished (built to {os.path.join(PROGRAM_FOLDER,'dist','xl-converter')})")
+    print(f"[Building] Finished (built to dist/{PROGRAM_NAME})")
