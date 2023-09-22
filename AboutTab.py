0c9edb3617920b72eef2b9082108641fd1093499
JacobDev1 <contact@codepoems.eu>
2023-09-22 05:48:16 +0200

    Refactor AboutTab

--- AboutTab.py
+++ AboutTab.py
@@ -5,7 +5,8 @@ from PySide6.QtWidgets import(
     QGridLayout,
     QHBoxLayout,
     QLabel,
-    QPushButton
+    QPushButton,
+    QSizePolicy
 )
 
 from PySide6.QtCore import(
@@ -21,18 +22,26 @@ class AboutTab(QWidget):
     def __init__(self):
         super(AboutTab, self).__init__()
 
-        about_tab_lt = QGridLayout()
-        self.setLayout(about_tab_lt)
+        tab_lt = QGridLayout()
+        self.setLayout(tab_lt)
 
-        # Left Side
+        # Label
         credits_l = QLabel(f"<h3>XL Converter</h3>Version {VERSION}<br>contact@codepoems.eu<br>XL Converter is licensed under <a href=\"LICENSE.txt\">GPL v3</a><br><a href=\"LICENSE_3RD_PARTY.txt\">3rd party licenses")
         credits_l.setAlignment(Qt.AlignCenter)
         credits_l.setOpenExternalLinks(True)
-        about_tab_lt.addWidget(credits_l,0,0)
 
-        # Right Side
-        about_tab_btn_lt = QHBoxLayout()
-        about_tab_btn_lt.addWidget(QPushButton("Donate", clicked=lambda: QDesktopServices.openUrl(QUrl("https://codepoems.eu/donate"))))
-        about_tab_btn_lt.addWidget(QPushButton("My Website", clicked=lambda: QDesktopServices.openUrl(QUrl("https://codepoems.eu"))))
+        # Buttons
+        donate_btn = QPushButton("Donate", clicked=lambda: QDesktopServices.openUrl(QUrl("https://codepoems.eu/donate")))
+        website_btn = QPushButton("Website", clicked=lambda: QDesktopServices.openUrl(QUrl("https://codepoems.eu")))
 
-        about_tab_lt.addLayout(about_tab_btn_lt,0,1)
+        # Positions
+        tab_lt.addWidget(credits_l,0,0,1,0)
+        tab_lt.addWidget(donate_btn,1,0)
+        tab_lt.addWidget(website_btn,1,1)
+
+        # Size Policy
+        tab_lt.setVerticalSpacing(20)
+        tab_lt.setAlignment(Qt.AlignCenter)
+
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.setMaximumSize(900, 300)
