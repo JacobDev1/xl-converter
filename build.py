441daa796018f8af83704aa7e0a748a6ff031fd5
JacobDev1 <contact@codepoems.eu>
2023-11-19 11:37:43 +0100

    Refactor build.py

--- build.py
+++ build.py
@@ -1,6 +1,3 @@
-# Multiplatform makefile replacement
-# I got fed up with trying to get `make` to work on Windows
-
 import platform, os, shutil, glob, subprocess, PyInstaller.__main__, argparse, shutil, stat
 from VARIABLES import VERSION
 
@@ -35,6 +32,15 @@ def copy(src, dst):
         shutil.copy(src, dst)
     except OSError as err:
         print(f"[Error] Copying failed ({src} -> {dst}) ({err})")
+
+def move(src, dst):
+    src = os.path.normpath(src)
+    dst = os.path.normpath(dst)
+
+    try:
+        shutil.move(src, dst)
+    except OSError as err:
+        print(f"[Error] Moving failed ({src} -> {dst}) ({err})")
 
 def copyTree(src, dst):
     src = os.path.normpath(src)
@@ -82,113 +88,179 @@ class Args():
     def getArg(self, arg):
         return self.args[arg]
 
-if __name__ == '__main__':
-    args = Args()
+class Builder():
+    # You can use "/" paths (the functions will normalize it)
+    def __init__(self):
+        self.args = Args()
 
-    # Clean-up
-    rmTree("dist")
+        # General
+        self.project_name = "xl-converter" 
+        self.dst_dir = "dist"
+        self.internal_dir = f"{self.dst_dir}/{self.project_name}/_internal"
 
-    # Prevent conflicts If the same folder is used on multiple systems
-    if os.path.isdir("build"):  
-        if os.path.isfile("build/last_built_on"):
-            last_built_on = open("build/last_built_on","r")
-            last_platform = last_built_on.read()
-            last_built_on.close()
-            
-            if last_platform == f"{platform.system()}_{platform.architecture()}":
-                print("[Building] Platform matches with previously compiled cache")
-            else:
-                print("[Building] Platform mismatch - deleting the cache")
-                rmTree("build") 
-                rmTree("__pycache__")
-        else:
-            print("[Building] \"last_built_on\" not found - deleting the cache")
-            rmTree("build")
-            rmTree("__pycache__")
+        # Shared
+        self.bin_dir = {
+            "Windows": "bin/win",
+            "Linux": "bin/linux"
+        }
 
-    # Preapre Directory and Build Binaries
-    print("[Building] Generating binaries")
-    makedirs("dist")
-    PyInstaller.__main__.run([
-        'main.spec'
-    ])
+        self.installer_path = {
+            "Windows": "misc/install.iss",
+            "Linux": "misc/install.sh"
+        }
 
-    # Copy Dependencies
-    print("[Building] Copying dependencies")
-    if platform.system() == "Windows":
-        copyTree("bin/win/", f"dist/xl-converter/_internal/bin/win")
-    elif platform.system() == "Linux":
-        copyTree("bin/linux/", f"dist/xl-converter/_internal/bin/linux")
+        self.misc_path = (
+            "LICENSE.txt",
+            "LICENSE_3RD_PARTY.txt"
+        )
 
-    # Append an Installer
-    if platform.system() == "Linux":
-        if args.getArg("app_image") == False:
-            print("[Building] Appending an installer script")
-            copy("misc/install.sh","dist")
-        print("[Building] Appending a desktop entry")
-        copy("misc/xl-converter.desktop","dist")
-    elif platform.system() == "Windows":
-        print("[Building] Appending an installer script")
-        copy("misc/install.iss","dist")
+        self.icon_svg_path = "icons/logo.svg"
 
-    # Embed Version into an Installer
-    if platform.system() == "Linux" and args.getArg("app_image") == False:
-        print("[Building] Embedding version into an installer script")
-        replaceLine(f"dist/install.sh", "VERSION=", f"VERSION=\"{VERSION}\"\n")
-    elif platform.system() == "Windows":
-        print("[Building] Embedding version into an installer script")
-        replaceLine(f"dist/install.iss", "#define MyAppVersion", f"#define MyAppVersion \"{VERSION}\"\n")
-        replaceLine(f"dist/install.iss", "OutputBaseFilename=", f"OutputBaseFilename=xl-converter-win-{VERSION}-x86_64\n")
+        # Linux
+        self.desktop_entry_path = "misc/xl-converter.desktop"
+        self.version_file_path = "misc/version.json"
+        self.appimagetool_path = "misc/appimagetool"
 
-    # Append misc.
-    print("[Building] Appending an icon and license files")
-    copy("LICENSE.txt", "dist/xl-converter/_internal")
-    copy("LICENSE_3RD_PARTY.txt", "dist/xl-converter/_internal")
-    makedirs(f"dist/xl-converter/_internal/icons")
-    copy("icons/logo.svg", f"dist/xl-converter/_internal/icons/logo.svg")
-
-    # Append an update file
-    print("[Building] Appending an update file (to place on a server)")
-    copy("misc/version.json", "dist")
-    replaceLine("dist/version.json", "latest_version", f"    \"latest_version\": \"{VERSION}\",\n")
-
-    # Append redistributables
-    if platform.system() == "Windows":
-        print("[Building] Appending redistributables")
-        makedirs(f"dist/xl-converter/redistributables")
-        copy("misc/VC_redist.x64.exe", f"dist/xl-converter/redistributables")
-        # Needed for ImageMagick
-        # https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170
-
-    # Build AppImage
-    if args.getArg("app_image"):
-        replaceLine("dist/xl-converter.desktop", "Icon=", "Icon=/logo\n")
-        replaceLine("dist/xl-converter.desktop", "Path=", "")
-        replaceLine("dist/xl-converter.desktop", "Exec=", "Exec=AppRun\n")
+        # Windows
+        self.redist_path = "misc/VC_redist.x64.exe"     # Needed for ImageMagick to work
         
-        makedirs(f"dist/AppDir/usr/bin")
-        copy("icons/logo.svg", "dist/AppDir")
-        shutil.move("dist/xl-converter.desktop", "dist/AppDir/xl-converter.desktop")
-        with open("dist/AppDir/AppRun", "w") as f:
+        # Build Names
+        self.build_inno_name = f"xl-converter-win-{VERSION}-x86_64"
+        self.build_7z_name = f"xl-converter-linux-{VERSION}-x86_64"
+        self.build_appimage_name = f"xl-converter-linux-{VERSION}-x86_64.AppImage"
+    
+    def build(self):
+        self._prepare()
+        self._buildBinaries()
+        self._copyDependencies()
+        self._appendInstaller()
+        self._appendDesktopEntry()
+        self._appendMisc()
+        self._appendRedistributables()
+        self._appendUpdateFile()
+        self._finish()
+
+        if self.args.getArg("app_image"):
+            self._buildAppImage()
+        elif self.args.getArg("pack"):
+            self._build7z()
+
+    def _prepare(self):
+        rmTree(self.dst_dir)
+
+        # Prevent conflicts If the same folder is used on multiple systems
+        if os.path.isdir("build"):  
+            if os.path.isfile("build/last_built_on"):
+                last_built_on = open("build/last_built_on","r")
+                last_platform = last_built_on.read()
+                last_built_on.close()
+                
+                if last_platform == f"{platform.system()}_{platform.architecture()}":
+                    print("[Building] Platform matches with previously compiled cache")
+                else:
+                    print("[Building] Platform mismatch - deleting the cache")
+                    rmTree("build") 
+                    rmTree("__pycache__")
+            else:
+                print("[Building] \"last_built_on\" not found - deleting the cache")
+                rmTree("build")
+                rmTree("__pycache__")
+        
+    def _buildBinaries(self):
+        print("[Building] Generating binaries")
+        makedirs(self.dst_dir)
+        PyInstaller.__main__.run([
+            'main.spec'
+        ])
+    
+    def _copyDependencies(self):
+        print("[Building] Copying dependencies")
+        bin_dir = self.bin_dir[platform.system()]
+        copyTree(bin_dir, f"{self.internal_dir}/{bin_dir}")
+    
+    def _appendInstaller(self):
+        installer_dir = self.installer_path[platform.system()]
+        installer_file = os.path.basename(installer_dir)
+
+        match platform.system():
+            case "Linux":
+                if self.args.getArg("app_image") == False:
+                    print("[Building] Appending an installer script")
+                    copy(installer_dir, self.dst_dir)
+                    if self.args.getArg("app_image") == False:
+                        print("[Building] Embedding version into an installer script")
+                        replaceLine(f"{self.dst_dir}/{installer_file}", "VERSION=", f"VERSION=\"{VERSION}\"\n")
+            case "Windows":
+                print("[Building] Appending an installer script")
+                copy(installer_dir, self.dst_dir)
+                print("[Building] Embedding version into an installer script")
+                replaceLine(f"{self.dst_dir}/{installer_file}", "#define MyAppVersion", f"#define MyAppVersion \"{VERSION}\"\n")
+                replaceLine(f"{self.dst_dir}/{installer_file}", "OutputBaseFilename=", f"OutputBaseFilename={self.build_inno_name}\n")
+    
+    def _appendDesktopEntry(self):
+        if platform.system() == "Linux":
+            print("[Building] Appending a desktop entry")
+            copy(self.desktop_entry_path, self.dst_dir)
+    
+    def _appendMisc(self):
+        print("[Building] Appending an icon and license files")
+        for i in self.misc_path:
+            copy(i, self.internal_dir)
+        makedirs(f"{self.internal_dir}/icons")
+        copy(self.icon_svg_path, f"{self.internal_dir}/icons/{os.path.basename(self.icon_svg_path)}")
+
+    def _appendRedistributables(self):
+        if platform.system() != "Windows":
+            return
+
+        print("[Building] Appending redistributables")
+        redist_dst = f"{self.dst_dir}/{self.project_name}/redistributables"
+        makedirs(redist_dst)
+        copy(self.redist_path, redist_dst)
+    
+    def _appendUpdateFile(self):
+        print("[Building] Appending an update file (to place on a server)")
+        copy(self.version_file_path, self.dst_dir)
+        replaceLine(f"{self.dst_dir}/{os.path.basename(self.version_file_path)}", "latest_version", f"    \"latest_version\": \"{VERSION}\",\n")
+    
+    def _finish(self):
+        with open("build/last_built_on","w") as last_built_on:
+            last_built_on.write(f"{platform.system()}_{platform.architecture()}")
+
+        print(f"[Building] Finished (built to {self.dst_dir}/{self.project_name})")
+
+    # _build methods transform the directory!
+    def _buildAppImage(self):
+        dsk_ent_f = os.path.basename(self.desktop_entry_path)
+        dsk_ent_p = f"{self.dst_dir}/{dsk_ent_f}"
+        appdir = f"{self.dst_dir}/AppDir"
+
+        replaceLine(dsk_ent_p, "Icon=", "Icon=/logo\n")
+        replaceLine(dsk_ent_p, "Path=", "")
+        replaceLine(dsk_ent_p, "Exec=", "Exec=AppRun\n")
+        
+        makedirs(f"{appdir}/usr/bin")
+        copy(self.icon_svg_path, appdir)
+        move(dsk_ent_p, f"{appdir}/{dsk_ent_f}")
+        with open(f"{appdir}/AppRun", "w") as f:
             f.write("#!/bin/bash\n\n")
-            f.write("exec ${APPDIR}/usr/bin/xl-converter/xl-converter $@")
-        addExecPerm(f"dist/AppDir/AppRun")
+            f.write(f"exec ${{APPDIR}}/usr/bin/{self.project_name}/{self.project_name} $@")
+        addExecPerm(f"{appdir}/AppRun")
 
-        shutil.move(f"dist/xl-converter", "dist/AppDir/usr/bin")
-        subprocess.run(("misc/appimagetool", "dist/AppDir", f"dist/xl-converter-{VERSION}-x86_64.AppImage"))
+        move(f"{self.dst_dir}/{self.project_name}", f"{appdir}/usr/bin")    # Move the whole project folder
+        subprocess.run((self.appimagetool_path, appdir, f"{self.dst_dir}/{self.build_appimage_name}"))
 
-    # Pack 7z
-    if args.getArg("pack"):
-        dst_direct = f"xl-converter-linux-{VERSION}-x86_64"
-        dst = f"dist/{dst_direct}"
+    def _build7z(self):
+        dst_direct = self.build_7z_name
+        dst = f"{self.dst_dir}/{self.build_7z_name}"
         makedirs(dst)
-        shutil.move("dist/xl-converter", dst)
-        shutil.move("dist/install.sh", dst)
-        shutil.move("dist/xl-converter.desktop", dst)
-        subprocess.run(("7z", "a", f"{dst_direct}.7z", dst_direct), cwd="dist")
 
-    # Log Last Build Platform
-    with open("build/last_built_on","w") as last_built_on:
-        last_built_on.write(f"{platform.system()}_{platform.architecture()}")
+        move(f"{self.dst_dir}/{self.project_name}", dst)
+        move(f"{self.dst_dir}/{os.path.basename(self.installer_path['Linux'])}", dst)
+        move(f"{self.dst_dir}/{os.path.basename(self.desktop_entry_path)}", dst)
+        subprocess.run(("7z", "a", f"{dst_direct}.7z", dst_direct), cwd=self.dst_dir)
 
-    print(f"[Building] Finished (built to dist/xl-converter)")
+
+if __name__ == '__main__':
+    builder = Builder()
+    builder.build()
