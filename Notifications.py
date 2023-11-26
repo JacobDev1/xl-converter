b680a7c84bbba1bb67a78c002e0bc67d48df8313
JacobDev1 <contact@codepoems.eu>
2023-11-26 04:57:00 +0100

    Reset notifications

--- Notifications.py
+++ Notifications.py
@@ -15,13 +15,14 @@ class Notifications():
         self.dlg.setWindowIcon(QIcon(ICON_SVG))
     
     def notify(self, title, msg):
-        self.dlg.setWindowTitle(title)
         self.dlg.setDetailedText(None)
+        self.dlg.setWindowTitle(title)
         self.dlg.setText(msg)
         return self.dlg.exec()
 
     def notifyDetailed(self, title, msg, details):
+        self.dlg.setDetailedText(None)  # Resets the state of the "Show Details..." button
+        self.dlg.setDetailedText(details)
         self.dlg.setWindowTitle(title)
         self.dlg.setText(msg)
-        self.dlg.setDetailedText(details)
         return self.dlg.exec()
