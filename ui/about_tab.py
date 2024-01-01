533e6ffd50963cb602ebc792b1cb635c916fde84
JacobDev1 <contact@codepoems.eu>
2024-01-01 18:56:50 +0100

    Refactor about_tab credits

--- ui/about_tab.py
+++ ui/about_tab.py
@@ -27,20 +27,22 @@ class AboutTab(QWidget):
         self.update_checker = UpdateChecker()
 
         # Label
-        credits_l = QLabel(f"<h3>XL Converter</h3>Version {VERSION}<br>contact@codepoems.eu<br>XL Converter is licensed under <a href=\"{QUrl.fromLocalFile(LICENSE_PATH).toString()}\">GPL v3</a><br><a href=\"{QUrl.fromLocalFile(LICENSE_3RD_PARTY_PATH).toString()}\">3rd party licenses")
+        credits_l = QLabel(f"""
+            <h3><a href=\"https://codepoems.eu/xl-converter\">XL Converter</a></h3>
+            Version {VERSION}<br>
+            <a href=\"mailto:contact@codepoems.eu\">contact@codepoems.eu</a><br>
+            XL Converter is licensed under <a href=\"{QUrl.fromLocalFile(LICENSE_PATH).toString()}\">GPL v3</a>
+            <br><a href=\"{QUrl.fromLocalFile(LICENSE_3RD_PARTY_PATH).toString()}\">3rd party licenses
+        """)
         credits_l.setAlignment(Qt.AlignCenter)
         credits_l.setOpenExternalLinks(True)
 
         # Buttons
-        donate_btn = QPushButton("Donate", clicked=lambda: QDesktopServices.openUrl(QUrl("https://codepoems.eu/donate")))
-        website_btn = QPushButton("Website", clicked=lambda: QDesktopServices.openUrl(QUrl("https://codepoems.eu")))
-        self.update_btn = QPushButton("Check for an Update", clicked=self.checkForUpdate)
+        self.update_btn = QPushButton("Check for Updates", clicked=self.checkForUpdate)
         self.update_checker.finished.connect(lambda: self.update_btn.setEnabled(True))
 
         # Positions
         tab_lt.addWidget(credits_l,0,0,1,0)
-        tab_lt.addWidget(donate_btn,1,0)
-        tab_lt.addWidget(website_btn,1,1)
         tab_lt.addWidget(self.update_btn, 2, 0, 1, 0)
 
         # Size Policy
