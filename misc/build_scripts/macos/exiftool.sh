71c135a010efbc50865c0423a1cf32461a33648e
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2025-10-31 11:12:44 +0100

    Refactor ExifTool macOS build script

--- misc/build_scripts/macos/exiftool.sh
+++ misc/build_scripts/macos/exiftool.sh
@@ -16,12 +16,6 @@ trap 'cleanup "${TEMP_DIR}"' EXIT
 check_env
 check_commands curl git
 
-export MACOSX_DEPLOYMENT_TARGET=11.0
-export ARCHFLAGS="-arch x86_64 -arch arm64"
-export CFLAGS="${ARCHFLAGS}"
-export CXXFLAGS="${ARCHFLAGS}"
-export LDFLAGS="${ARCHFLAGS}"
-
 # Bootstrap cpanm and download ExifTool
 curl -fsSL https://cpanmin.us -o "${CPANM_PATH}"
 chmod +x "${CPANM_PATH}"
@@ -29,47 +23,91 @@ git clone --depth 1 -b "${EXIFTOOL_TAG}" https://github.com/exiftool/exiftool "$
 cd "${TEMP_DIR}/exiftool"
 chmod +x ./build_tag_lookup && ./build_tag_lookup
 
-if ! file -b "${PERL_PATH}" | grep -qi "universal binary"; then
-  echo "Perl is not universal. Reinstall it with +universal, and try again."
-  exit 1
-fi
-
 # Packages to include in the bundle
 # Reference: https://github.com/exiftool/exiftool/blob/master/META.json
 perl_packages=(
-  IO::Compress::Brotli IO::Uncompress::Brotli Archive::Zip Compress::Zlib IO::Compress::Bzip2 IO::Compress::RawDeflate IO::Uncompress::RawInflate Compress::Raw::Lzma
-  Digest::SHA Digest::MD5 Digest::SHA Time::Piece POSIX::strptime Time::HiRes
-  Unicode::LineBreak
+    IO::Compress::Brotli IO::Uncompress::Brotli Archive::Zip Compress::Zlib IO::Compress::Bzip2 IO::Compress::RawDeflate IO::Uncompress::RawInflate Compress::Raw::Lzma
+    Digest::SHA Digest::MD5 Digest::SHA Time::Piece POSIX::strptime Time::HiRes
+    Unicode::LineBreak
 )
 
-cpan_prefix="${TEMP_DIR}/vendor"
-mkdir -p "${cpan_prefix}"
-"${PERL_PATH}" \
-  "${CPANM_PATH}" \
-  -L "${cpan_prefix}" \
-  --notest --quiet --no-interactive \
-  PAR::Packer Module::ScanDeps \
-  "${perl_packages[@]}"
+build_exiftool() {
+    local target_arch="$1"
+    local output="$2"
+    local cpan_prefix="${TEMP_DIR}/vendor_${target_arch}"
 
-export PERL5LIB="${cpan_prefix}/lib/perl5"
-export PATH="${cpan_prefix}/bin:${PATH}"
+    export MACOSX_DEPLOYMENT_TARGET=11.0
 
-pp_packages=()
-for p in "${perl_packages[@]}"; do
-  pp_packages+=(-M "$p")
-done
+    mkdir -p "${cpan_prefix}"
+    arch -"${target_arch}" \
+        "${PERL_PATH}" \
+        "${CPANM_PATH}" \
+        -L "${cpan_prefix}" \
+        --notest --quiet --no-interactive \
+        PAR::Packer Module::ScanDeps \
+        "${perl_packages[@]}"
 
-"${PERL_PATH}" \
-  -S pp \
-  -I lib -a "lib/=lib" \
-  "${pp_packages[@]}" \
-  -o "${TEMP_DIR}/exiftool.universal" \
-  exiftool
+    export PERL5LIB="${cpan_prefix}/lib/perl5"
+    export PATH="${cpan_prefix}/bin:${PATH}"
 
-if ! file -b "${TEMP_DIR}/exiftool.universal" | grep -qi "universal binary"; then
-  echo "Warning, the bundle is not a universal binary."
-fi
+    local pp_packages=()
+    for p in "${perl_packages[@]}"; do
+        pp_packages+=(-M "$p")
+    done
+
+    arch -"${target_arch}" \
+        "${PERL_PATH}" \
+        -S pp \
+        -I lib -a "lib/" \
+        "${pp_packages[@]}" \
+        -o "${output}" \
+        exiftool
+}
+
+is_rosetta_available() {
+    [ "$(uname -m)" != "arm64" ] && return 0
+    /usr/bin/arch -x86_64 /usr/bin/true > /dev/null 2>&1 && return 0
+    return 1
+}
+
+pull_arm64_prebuild() {
+    # TODO
+    local output="$1"
+}
+
+x86_64_slice="${TEMP_DIR}/exiftool.x86_64"
+arm64_slice="${TEMP_DIR}/exiftool.arm64"
+exiftool_bin="${TEMP_DIR}/exiftool_build_export"
+
+case "$(uname -m)" in
+    x86_64)
+        build_exiftool x86_64 "${x86_64_slice}"
+        # pull_arm64_prebuild "${arm64_slice}"
+        # lipo -create "${x86_64_slice}" "${arm64_slice}" -output "${exiftool_bin}"
+        mv "$x86_64_slice" "$exiftool_bin"
+        echo "Generated binary will be thin. This script is a work-in-progress."
+        ;;
+    arm64)
+        if is_rosetta_available; then
+            build_exiftool x86_64 "${x86_64_slice}"
+            build_exiftool arm64 "${arm64_slice}"
+            lipo -create "${x86_64_slice}" "${arm64_slice}" -output "${exiftool_bin}"
+        elif [[ "${THIN_BINARY:-0}" != "1" ]]; then
+            warning "Install Rosetta and try again or export THIN_BINARY=1 to compile for native arch only."
+            exit 1
+        else
+            build_exiftool arm64 "${arm64_slice}"
+            mv "${arm64_slice}" "${exiftool_bin}"
+            warning "The exiftool binary will not run on Intel macOS!"
+        fi
+        ;;
+    *)
+        echo "Unsupported host architecture"
+        exit 1
+        ;;
+esac
+
 
 mkdir -p "${OUTPUT_DIR}"
-mv "${TEMP_DIR}/exiftool.universal" "${OUTPUT_DIR}/exiftool"
-echo "Build artifacts copied to: ${OUTPUT_DIR}"
+mv "${exiftool_bin}" "${OUTPUT_DIR}/exiftool"
+echo "The build artifact copied to: ${OUTPUT_DIR}"
