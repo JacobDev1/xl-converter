227d8ec0a769d446c0b20a71bfa287ee661266c0
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2025-12-09 02:04:06 +0100

    Fix libaom toolchain on macOS

--- misc/build_scripts/macos/libavif.sh
+++ misc/build_scripts/macos/libavif.sh
@@ -60,7 +60,7 @@ for arch in x86_64 arm64; do
 
     if [ "${arch}" = "x86_64" ]; then
         cmake_flags+=(
-            -DCMAKE_TOOLCHAIN_FILE="${TEMP_DIR}/libavif/ext/aom/build/cmake/toolchains/x86-macos.cmake"
+            -DCMAKE_TOOLCHAIN_FILE="${TEMP_DIR}/libavif/ext/aom/build/cmake/toolchains/x86_64-macos.cmake"
             -DHAVE_NEON=0
         )
     fi
