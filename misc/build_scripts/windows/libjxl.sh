93b4e4483e25928c5c19bdc766099a3c3ddce0c5
JacobDev1 <65915437+JacobDev1@users.noreply.github.com>
2026-08-19 06:22:22 +0200

    chore: update libjxl to v0.12.0 (Windows)

--- misc/build_scripts/windows/libjxl.sh
+++ misc/build_scripts/windows/libjxl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-LIBJXL_TAG="v0.11.1"
+LIBJXL_TAG="v0.12.0"
+JPEGLI_COMMIT="031a0077f5799a6041004267fc12b956c1f52a20"  # Full commit hash
 RUN_DIR=$(pwd)
 OUTPUT_DIR="${RUN_DIR}/bin/win/libjxl"
 TEMP_DIR=$(mktemp -d)
@@ -22,7 +23,7 @@ check_packages \
     mingw-w64-x86_64-libpng \
     mingw-w64-x86_64-libjpeg-turbo
 
-# Build
+# Build libjxl
 cd "${TEMP_DIR}"
 git clone --depth 1 -b "${LIBJXL_TAG}" https://github.com/libjxl/libjxl.git libjxl
 cd libjxl/
@@ -44,11 +45,44 @@ cmake -DCMAKE_BUILD_TYPE=Release \
     -DJPEGXL_ENABLE_DEVTOOLS=OFF \
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
     ..
-cmake --build .
-
-# Bundle
+cmake --build . --parallel
 mkdir -p "${OUTPUT_DIR}"
 cd tools/
-cp cjpegli.exe cjxl.exe djxl.exe jxlinfo.exe "${OUTPUT_DIR}"
+cp cjxl.exe djxl.exe jxlinfo.exe "${OUTPUT_DIR}"
+
+# Build jpegli
+cd "${TEMP_DIR}"
+git init jpegli
+cd jpegli
+git remote add origin https://github.com/google/jpegli.git
+git fetch --depth 1 origin ${JPEGLI_COMMIT}
+git switch --detach ${JPEGLI_COMMIT}
+git submodule update --init --recursive --depth 1 --recommend-shallow \
+    third_party/highway \
+    third_party/lcms \
+    third_party/libpng \
+    third_party/zlib \
+    third_party/libjpeg-turbo
+cmake -S . -B build/ \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DJPEGLI_STATIC=ON \
+    -DBUILD_TESTING=OFF \
+    -DJPEGLI_ENABLE_TOOLS=ON \
+    -DJPEGLI_ENABLE_JPEGLI_LIBJPEG=OFF \
+    -DJPEGLI_ENABLE_BENCHMARK=OFF \
+    -DJPEGLI_ENABLE_FUZZERS=OFF \
+    -DJPEGLI_ENABLE_JNI=OFF \
+    -DJPEGLI_ENABLE_SJPEG=OFF \
+    -DJPEGLI_ENABLE_OPENEXR=OFF \
+    -DJPEGLI_ENABLE_DEVTOOLS=OFF \
+    -DJPEGLI_ENABLE_DOXYGEN=OFF \
+    -DJPEGLI_BUNDLE_LIBPNG=ON
+cmake --build build/ \
+    --target cjpegli \
+    --parallel
+cp build/tools/cjpegli.exe "${OUTPUT_DIR}"
+
+# Bundle
 bundle_dlls "${OUTPUT_DIR}"
 echo "Binaries copied to: ${OUTPUT_DIR}"
